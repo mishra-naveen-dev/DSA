225947bab0e373a30e730d9877b9847fd13b1e3e
nvineng <naveen211063cse@sdbc.ac.in>
2024-01-08 20:14:25 +0530

    first commit

---
+++ hello/TimespaceT.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, n = 15;
+    for (i = 1; i <= n; i = i * 3)
+    {
+        /* code */
+        cout << "Hello world" << endl;
+    }
+    return 0;
+}
