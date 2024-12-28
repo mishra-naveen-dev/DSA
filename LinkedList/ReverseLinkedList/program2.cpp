123723ecf8b54326be75d0da773b114bb136f90f
nvineng <naveen211063cse@sdbc.ac.in>
2024-12-28 15:18:23 +0530

    LinkedList/ReverseLinkedList program

---
+++ LinkedList/ReverseLinkedList/program2.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int new_data)
+    {
+        data = new_data;
+        next = nullptr;
+    }
+};
+Node *reverseList(Node *head)
+{
+    Node *curr = head, *prev = nullptr, *next;
+    while (curr != nullptr)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
+void printList(Node *node)
+{
+    while (node != nullptr)
+    {
+        cout << " " << node->data;
+        node = node->next;
+    }
+}
+
+int main()
+{
+    Node *head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+    cout << "Given Linked List: ";
+    printList(head);
+    head = reverseList(head);
+    cout << "\nReversed Linked List: ";
+    printList(head);
+    return 0;
+}
